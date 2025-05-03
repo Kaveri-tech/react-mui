d29937ab2ad3cbe5ebe8ca55fed958a3d4106c42
Kaveri-tech <kaverikharat99@gmail.com>
2025-05-03 14:49:50 +0530

    Add initial files

---
+++ src/App.jsx
@@ -0,0 +1,128 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import ButtonBase from '@mui/material/ButtonBase';
+import Typography from '@mui/material/Typography';
+
+const images = [
+  {
+    url: 'https://cdn.pixabay.com/photo/2016/08/07/15/02/blueberry-1576407_1280.jpg',
+    title: 'Breakfast',
+    width: '40%',
+  },
+  {
+    url: 'https://static.vecteezy.com/system/resources/previews/023/010/419/non_2x/the-delicious-burger-in-the-black-background-with-ai-generated-free-photo.jpg',
+    title: 'Burgers',
+    width: '30%',
+  },
+  {
+    url: 'https://images.pexels.com/photos/212372/pexels-photo-212372.jpeg?cs=srgb&dl=pexels-asphotograpy-212372.jpg&fm=jpg',
+    title: 'Camera',
+    width: '30%',
+  },
+];
+
+const ImageButton = styled(ButtonBase)(({ theme }) => ({
+  position: 'relative',
+  height: 200,
+  [theme.breakpoints.down('sm')]: {
+    height: 120,
+  },
+  '&:hover, &.Mui-focusVisible': {
+    zIndex: 1,
+    '& .MuiImageBackdrop-root': {
+      opacity: 0.15,
+    },
+    '& .MuiImageMarked-root': {
+      opacity: 0,
+    },
+    '& .MuiTypography-root': {
+      border: '4px solid currentColor',
+    },
+  },
+}));
+
+const ImageSrc = styled('span')({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center 40%',
+});
+
+const Image = styled('span')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: theme.palette.common.white,
+}));
+
+const ImageBackdrop = styled('span')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundColor: theme.palette.common.black,
+  opacity: 0.4,
+  transition: theme.transitions.create('opacity'),
+}));
+
+const ImageMarked = styled('span')(({ theme }) => ({
+  height: 3,
+  width: 18,
+  backgroundColor: theme.palette.common.white,
+  position: 'absolute',
+  bottom: -2,
+  left: 'calc(50% - 9px)',
+  transition: theme.transitions.create('opacity'),
+}));
+
+export default function ButtonBaseDemo() {
+  return (
+    <>
+      <center><h1>MATERIAL UI</h1></center>
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+          minWidth: 300,
+          width: '100%',
+        }}
+      >
+        {images.map((image) => (
+          <Box key={image.title} sx={{ p: 1, width: { xs: '100%', sm: image.width } }}>
+            <ImageButton focusRipple sx={{ width: '100%' }}>
+              <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
+              <ImageBackdrop className="MuiImageBackdrop-root" />
+              <Image>
+                <Typography
+                  component="span"
+                  variant="subtitle1"
+                  color="inherit"
+                  sx={(theme) => ({
+                    position: 'relative',
+                    p: 4,
+                    pt: 2,
+                    pb: `calc(${theme.spacing(1)} + 6px)`,
+                  })}
+                >
+                  {image.title}
+                  <ImageMarked className="MuiImageMarked-root" />
+                </Typography>
+              </Image>
+            </ImageButton>
+          </Box>
+        ))}
+      </Box>
+    </>
+  );
+}
